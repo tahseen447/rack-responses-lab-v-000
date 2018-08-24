7907fbe5a42d94530ba02d181d3d0e1f681ac525
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 18:54:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call (env)
     resp = Rack::Response.new
     time = Time.new
-    
+
     puts "#{time}"
-    puts "#{time.localtime}"
+    puts "#{time.ctime}"
     if hour < 12
       resp.write("Good Morning!")
     else
